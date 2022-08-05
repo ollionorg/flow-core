b4d902707b1031a8ba521e7032c07f88388b95e1
vikas-cldcvr <vikas@cldcvr.com>
2022-08-05 15:44:47 +0530

    FLOW-330 publish workflow added

--- packages/flow-core/CHANGELOG.md
+++ packages/flow-core/CHANGELOG.md
@@ -1,6 +1,9 @@
 
 # Change Log
 
+## [0.0.8] - 2022-08-05
+### Improvements
+- publish github action created
 ## [0.0.7] - 2022-08-05
 ### Bug Fixes
 - `f-button` `shape` attribute removed.
--- stories/f-button/f-button-states-sizes.template.ts
+++ stories/f-button/f-button-states-sizes.template.ts
@@ -5,6 +5,28 @@ const StatesAndSizesTemplate: Story<unknown> = () => {
   const states = ["primary", "neutral", "success", "warning", "danger"];
   const sizes = ["large", "medium", "small", "x-small"];
   return html`
+    <f-button label="add" icon-right="i-crown" size="medium"></f-button>
+    <f-button label="add" icon-left="i-crown" size="medium"></f-button>
+    <f-button
+      label="add"
+      icon-left="i-plus"
+      icon-right="i-user"
+      shape="block"
+      size="medium"
+      variant="round"
+    ></f-button>
+    <br />
+    <br />
+    <f-button
+      label="add"
+      icon-left="i-plus"
+      icon-right="i-user"
+      shape="block"
+      size="medium"
+      variant="block"
+    ></f-button>
+    <br />
+    <br />
     ${states.map(
       (state) =>
         html`<f-button label="label" .state=${state}></f-button> &nbsp;`

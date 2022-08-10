ddc7d06fcc3b8cb88f650e4fa98b188c5f34d317
vikas-cldcvr <vikas@cldcvr.com>
2022-08-10 23:38:07 +0530

    FLOW-345 f-text added

--- packages/flow-core/src/components/f-text/f-text.ts
+++ packages/flow-core/src/components/f-text/f-text.ts
@@ -79,7 +79,42 @@ export class FText extends FElement {
   @property({ reflect: true, type: Boolean })
   expandable?: boolean = false;
 
+  protected _contentBeforeFocus = "";
+  protected value = "";
+
+  constructor() {
+    super();
+    this.addEventListener("focusin", this._handleFocusIn);
+    this.addEventListener("keyup", this._handleKeyUp);
+  }
+  _handleFocusIn() {
+    if (this.editable) {
+      this._contentBeforeFocus = this.innerHTML;
+    }
+  }
+  _handleKeyUp(event: KeyboardEvent) {
+    if (this.editable) {
+      if (event.key === "Escape") {
+        this.blur();
+        this.innerHTML = this._contentBeforeFocus;
+      }
+      if (event.key === "Enter" && !event.shiftKey) {
+        this.value = this.innerHTML;
+        this.blur();
+        this.dispatchEvent(new Event("input", { bubbles: true }));
+      }
+    }
+  }
+  get isTextClamped() {
+    return this.scrollHeight > this.clientHeight && this.expandable;
+  }
   render() {
+    if (this.editable) {
+      this.contentEditable = "true";
+    } else {
+      this.contentEditable = "false";
+    }
+
     if (!this.weight) {
       if (this.variant === "heading") {
         this.weight = "bold";
@@ -87,6 +122,12 @@ export class FText extends FElement {
         this.weight = "regular";
       }
     }
+
+    // if (this.isTextClamped) {
+    //   const span = document.createElement("span");
+    //   span.innerHTML = "more";
+    //   this.appendChild(span);
+    // }
     return html``;
   }
 }
--- stories/f-text.stories.ts
+++ stories/f-text.stories.ts
@@ -49,3 +49,89 @@ const STemplate: Story<unknown> = () => {
 };
 
 export const states = STemplate.bind({});
+
+const ATemplate: Story<unknown> = () => {
+  const aligns = ["left", "center", "right"];
+
+  return html`<f-div variant="column" gap="large" width="100%">
+    ${aligns.map(
+      (a) =>
+        html` <f-text .align=${a}
+          >This is ${a} aligned.Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis
+          blandit. Vestibulum volutpat tellus diam, consequat gravida libero
+          rhoncus ut.</f-text
+        >`
+    )}
+  </f-div>`;
+};
+
+export const align = ATemplate.bind({});
+
+const LTemplate: Story<unknown> = () => {
+  return html`<f-div variant="column" gap="large" width="100%">
+    <f-text loading>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam
+      at erat pulvinar.
+    </f-text>
+  </f-div>`;
+};
+
+export const loading = LTemplate.bind({});
+
+const DTemplate: Story<unknown> = () => {
+  return html`<f-div variant="column" gap="large" width="100%">
+    <f-text disabled>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam
+      at erat pulvinar.
+    </f-text>
+  </f-div>`;
+};
+
+export const disabled = DTemplate.bind({});
+
+const ETemplate: Story<unknown> = () => {
+  const handleInput = (event: InputEvent) => {
+    console.log((event.target as HTMLInputElement)?.value);
+  };
+  return html`<f-div variant="column" gap="large" width="100%">
+    <f-text editable @input=${handleInput}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam
+      at erat pulvinar.
+    </f-text>
+  </f-div>`;
+};
+
+export const editable = ETemplate.bind({});
+
+const ELTemplate: Story<unknown> = () => {
+  return html`<f-div variant="column" gap="large" width="100%">
+    <f-text ellipsis>
+      <b> Lorem<sup>2</sup> ipsum dolor </b>sit amet,
+      <a href="#">here</a> consectetur adipiscing elit. <i>Etiam</i> semper diam
+      at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus
+      diam, consequat gravida libero rhoncus ut.Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at
+      pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida
+      libero rhoncus ut.
+    </f-text>
+    <f-text expandable>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam
+      at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus
+      diam, consequat gravida libero rhoncus ut.Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at
+      pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida
+      libero rhoncus ut.
+    </f-text>
+    <f-text state="success">
+      <ul>
+        <li>John Lennon</li>
+        <li>Paul McCartney</li>
+        <li>George Harrison</li>
+        <li>Ringo Starr</li>
+      </ul>
+    </f-text>
+  </f-div>`;
+};
+
+export const ellipsis = ELTemplate.bind({});

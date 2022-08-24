c45b4d94c26fef2efb35664a52fa99119f3134cb
Mayank fulera <fuleramayank@gmail.com>
2022-08-24 13:07:54 +0530

    divider usage story added

--- stories/f-divider.stories.ts
+++ stories/f-divider.stories.ts
@@ -7,9 +7,6 @@ export default {
 } as Meta;
 
 const Template: Story<unknown> = () => {
-  const variants = ["solid", "dashed", "dotted"];
-  const states = ["primary", "success", "warning", "danger", "neutral"];
-  const sizes = ["large", "medium"];
   return html`
     <br />
     <f-div width="500px" direction="column" padding="large" gap="large">
@@ -32,4 +29,74 @@ const Template: Story<unknown> = () => {
   `;
 };
 
+const DividerUsageTemplate: Story<unknown> = () => {
+  return html`
+    <f-div direction="column" width="100%" height="100%">
+      <f-div height="hug-content" gap="auto" padding="small">
+        <f-div gap="small" width="hug-content" align="middle-center">
+          <f-icon source="p-cloudcover-dark" size="large" state="primary"></f-icon>
+          <f-text variant="para" size="x-small" weight="regular"> Home </f-text>
+        </f-div>
+        <f-div gap="large" width="hug-content">
+          <f-icon .clickable=${true} source="i-bug" size="large" state="default"></f-icon>
+          <f-icon .clickable=${true} source="i-file" size="large" state="default"></f-icon>
+          <f-icon .clickable=${true} source="i-fire" size="large" state="default"></f-icon>
+          <f-icon .clickable=${true} source="i-alarm" size="large" state="default"></f-icon>
+          <f-icon .clickable=${true} source="i-org" size="large" state="default"></f-icon>
+          <f-icon .clickable=${true} source="i-user" size="large" state="default"></f-icon>
+        </f-div>
+      </f-div>
+      <f-divider variant="solid" size="medium" state="subtle"></f-divider>
+      <f-div gap="auto">
+        <f-div align="middle-center" width="49%" padding="large">
+          <f-text type="para" size="small"
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.</f-text
+          >
+        </f-div>
+        <f-divider variant="solid" size="medium" state="subtle"></f-divider>
+        <f-div align="middle-center" width="49%" padding="large">
+          <f-text type="para" size="small"
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.</f-text
+          >
+        </f-div>
+      </f-div>
+      <f-divider variant="dashed" size="medium" state="subtle"></f-divider>
+      <f-div gap="auto">
+        <f-div align="middle-center" width="49%" padding="large">
+          <f-text type="para" size="small"
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.</f-text
+          >
+        </f-div>
+        <f-divider variant="dashed" size="medium" state="subtle"></f-divider>
+        <f-div align="middle-center" width="49%" padding="large">
+          <f-text type="para" size="small"
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.</f-text
+          >
+        </f-div>
+      </f-div>
+    </f-div>
+  `;
+};
+
 export const basic = Template.bind({});
+export const DividerUsage = DividerUsageTemplate.bind({});

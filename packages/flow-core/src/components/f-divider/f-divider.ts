bdb8d3d70d4610990bfd649b472e82cef0c262ae
mayankfulera <fuleramayank@gmail.com>
2022-08-24 17:07:55 +0530

    FLOW-387 f-divider (#6)
    * divider addition
    
    * divider test addition
    
    * changes
    
    * divider usage story added
    
    * changes
    
    * changes
    
    * changes
    
    * changes scss
    
    * changes

---
+++ packages/flow-core/src/components/f-divider/f-divider.ts
@@ -0,0 +1,40 @@
+import { html, unsafeCSS } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import eleStyle from "./f-divider.scss";
+import { FElement } from "../../mixins/components/f-element/f-element";
+
+@customElement("f-divider")
+export class FDivider extends FElement {
+  /**
+   * css loaded from scss file
+   */
+  static styles = [unsafeCSS(eleStyle)];
+
+  /**
+   * @attribute The solid variant is the default.
+   */
+  @property({ reflect: true, type: String })
+  variant?: "solid" | "dashed" | "dotted" = "solid";
+
+  /**
+   * @attribute The medium size is the default.
+   */
+  @property({ reflect: true, type: String })
+  size?: "large" | "medium" = "medium";
+
+  /**
+   * @attribute The state of Divider helps in indicating the degree of emphasis. By default it is default state.
+   */
+  @property({ reflect: true, type: String })
+  state?: "default" | "secondary" | "subtle" = "default";
+
+  render() {
+    return html``;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "f-divider": FDivider;
+  }
+}

5c42cb0648ed670f22776b5a5d8b3e33c8657434
vikas-cldcvr <vikas@cldcvr.com>
2022-10-12 16:45:30 +0530

    FLOW-430 console errors fixed

---
+++ stories/utils/registerIcons.js
@@ -0,0 +1,17 @@
+import SystemIconPack from "@cldcvr/flow-system-icon/dist/types/icon-pack";
+import ProductIconPack from "@cldcvr/flow-product-icon/dist/types/icon-pack";
+import GcpIconPack from "@cldcvr/flow-gcp-icon/dist/types/icon-pack";
+import AwsIconPack from "@cldcvr/flow-aws-icon/dist/types/icon-pack";
+
+import { ConfigUtil } from "@cldcvr/flow-core/src/modules/config";
+
+ConfigUtil.setConfig({
+  iconPack: {
+    ...SystemIconPack,
+    ...ProductIconPack,
+    ...GcpIconPack,
+    ...AwsIconPack,
+  },
+});
+
+export {};

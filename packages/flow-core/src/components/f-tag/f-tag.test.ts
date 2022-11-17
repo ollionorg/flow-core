b5b7e43a15483ac391c3a467cdeb6bc40dbb10e6
Mayank fulera <fuleramayank@gmail.com>
2022-11-17 17:23:55 +0530

    f-tag added

---
+++ packages/flow-core/src/components/f-tag/f-tag.test.ts
@@ -0,0 +1,79 @@
+import { html, fixture, expect } from "@open-wc/testing";
+import IconPack from "@cldcvr/flow-system-icon/dist/types/icon-pack";
+
+// import flow-core elements
+import "@cldcvr/flow-core";
+
+import { FTag, FIcon, ConfigUtil, FCounter } from "@cldcvr/flow-core";
+// importing `loadingSVG` to cross check
+import loadingSVG from "../../mixins/svg/loader";
+
+// setting icon pack for testing icon related test cases
+ConfigUtil.setConfig({ iconPack: IconPack });
+
+describe("f-tag", () => {
+  it("is defined", () => {
+    const el = document.createElement("f-tag");
+    expect(el).instanceOf(FTag);
+  });
+  it("should render label in default slot", async () => {
+    const el = await fixture(html` <f-tag label="abc" state="neutral"></f-tag> `);
+    const descendant = el.shadowRoot!.querySelector(".f-tag")!;
+    expect(descendant.textContent?.trim()).to.equal("abc");
+  });
+  it("should throw error", async () => {
+    try {
+      await fixture(html` <f-tag state="neutral"></f-tag>`);
+    } catch (e) {
+      expect((e as Error).message).to.equal("f-tag : label OR icon-left is mandatory field");
+    }
+  });
+  it("should render neutral state", async () => {
+    const el = await fixture(html` <f-tag label="abc" state="neutral"></f-tag> `);
+    const descendant = el.shadowRoot!.querySelector(".f-tag")!;
+    expect(descendant.getAttribute("state")).to.equal("neutral");
+  });
+  it("should throw error", async () => {
+    try {
+      await fixture(html` <f-tag label="label"></f-tag>`);
+    } catch (e) {
+      expect((e as Error).message).to.equal("f-tag : state OR fill prop is mandatory field");
+    }
+  });
+  it("should render with all default properties", async () => {
+    const el = await fixture(html` <f-tag label="abc" state="neutral"></f-tag> `);
+    const descendant = el.shadowRoot!.querySelector(".f-tag")!;
+    expect(descendant.getAttribute("size")).to.equal("medium");
+  });
+  it("should render icon left", async () => {
+    const el = await fixture(
+      html` <f-tag label="abc" state="neutral" icon-left="i-plus"></f-tag> `
+    );
+    const descendant = el.shadowRoot!.querySelector(".f-tag")!;
+    const icon = descendant.children[0];
+    expect(icon).instanceOf(FIcon);
+  });
+  it("should render icon right", async () => {
+    const el = await fixture(
+      html` <f-tag label="abc" state="neutral" icon-right="i-plus"></f-tag> `
+    );
+    const descendant = el.shadowRoot!.querySelector(".f-tag")!;
+    const icon = descendant.children[0];
+    expect(icon).instanceOf(FIcon);
+  });
+  it("should render counter", async () => {
+    const el = await fixture(html` <f-tag label="abc" state="neutral" counter="88"></f-tag> `);
+    const descendant = el.shadowRoot!.querySelector(".f-tag")!;
+    const counter = descendant.children[descendant.children.length - 1];
+    expect(counter).instanceOf(FCounter);
+    const descCounter = counter.shadowRoot!.querySelector(".f-counter")!;
+    expect(descCounter.textContent?.trim()).equal("88");
+  });
+  it("should render loader", async () => {
+    const el = await fixture(html` <f-tag label="abc" state="neutral" loading></f-tag> `);
+    const descendant = el.shadowRoot!.querySelector(".f-tag")!;
+    const loading = descendant.children[0];
+    const svg = await fixture(loadingSVG);
+    expect(loading.outerHTML).equal(svg.outerHTML);
+  });
+});
---
+++ packages/flow-core/src/components/f-tag/f-tag.ts
@@ -0,0 +1,235 @@
+import { html, unsafeCSS } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { FRoot } from "../../mixins/components/f-root/f-root";
+import eleStyle from "./f-tag.scss";
+import { unsafeSVG } from "lit-html/directives/unsafe-svg.js";
+import loader from "../../mixins/svg/loader";
+import { classMap } from "lit-html/directives/class-map.js";
+import validateColor from "validate-color";
+
+/**
+ * @summary Buttons allow users to perform an action or to initiate a new function.
+ */
+@customElement("f-tag")
+export class FTag extends FRoot {
+  /**
+   * css loaded from scss file
+   */
+  static styles = [unsafeCSS(eleStyle)];
+
+  /**
+   * @attribute label property defines the text label on a tag. Label of a tag is always uppercase.
+   */
+  @property({ type: String })
+  label!: string;
+
+  /**
+   * @attribute category of tag
+   */
+  @property({ reflect: true, type: String })
+  fill?: string;
+
+  /**
+   * @attribute The medium size is the default and recommended option.
+   */
+  @property({ reflect: true, type: String })
+  size?: "large" | "medium" | "small" = "medium";
+
+  /**
+   * @attribute The states on tags are to indicate various degrees of emphasis of the action.
+   */
+  @property({ reflect: true, type: String })
+  state?: "primary" | "neutral" | "success" | "warning" | "danger" | "inherit";
+
+  /**
+   * @attribute Icon-left enables an icon on the left of the label of a tag.
+   */
+  @property({ reflect: true, type: String, attribute: "icon-left" })
+  iconLeft?: string;
+
+  /**
+   * @attribute Icon-right enables an icon on the right of the label of a tag.
+   */
+  @property({ reflect: true, type: String, attribute: "icon-right" })
+  iconRight?: string;
+
+  /**
+   * @attribute Counter property enables a counter on the tag.
+   */
+  @property({ reflect: true, type: Number })
+  counter?: string;
+
+  /**
+   * @attribute Loader icon replaces the content of the tag .
+   */
+  @property({ reflect: true, type: Boolean })
+  loading?: boolean = false;
+
+  /**
+   * @attribute Loader icon replaces the content of the tag .
+   */
+  @property({ reflect: true, type: Boolean })
+  selected?: boolean = false;
+
+  /**
+   * @attribute The disabled attribute can be set to keep a user from clicking on the tag.
+   */
+  @property({ reflect: true, type: Boolean })
+  disabled?: boolean = false;
+
+  /**
+   * compute icon size based on tag size
+   */
+  get iconSize() {
+    if (this.size === "large") {
+      return "medium";
+    } else if (this.size === "medium") {
+      return "small";
+    } else {
+      return "x-small";
+    }
+  }
+
+  /**
+   * render on playground
+   */
+  connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener("resize", () => this.requestUpdate());
+  }
+  disconnectedCallback() {
+    window.removeEventListener("resize", () => this.requestUpdate());
+    super.disconnectedCallback();
+  }
+
+  // apply inline styles to shadow-dom for custom fill prop
+  applyStyles() {
+    if (this.fill) {
+      if (this.selected) {
+        return `background: ${this.fill}; border: 1px solid ${this.fill}; filter: brightness(60%);`;
+      } else if (this.loading) {
+        return `background-color: ${this.fill}; border: 1px solid ${this.fill}; color: transparent;`;
+      } else {
+        return `background: ${this.fill}; border: 1px solid ${this.fill}`;
+      }
+    }
+    return "";
+  }
+
+  /**
+   * validation for all atrributes
+   */
+  validateProperties() {
+    const stateList = ["primary", "neutral", "success", "warning", "danger", "inherit"];
+    if (!this.label && !this.iconLeft) {
+      throw new Error("f-tag : label OR icon-left is mandatory field");
+    }
+    if (!stateList.includes(this.state ?? "") && !this.fill) {
+      throw new Error("f-tag : state OR fill prop is mandatory field");
+    }
+    if (this.fill && !validateColor(this.fill)) {
+      throw new Error("f-tag : enter correct color-name or hex-color-code");
+    }
+  }
+
+  render() {
+    this.validateProperties();
+    /**
+     * checks if host element's `:before` has shimmer by accessing  computedstyles
+     */
+    const hasShimmer = (getComputedStyle(this, "::before") as any)["animation-name"] === "shimmer";
+
+    /**
+     * if hasShimmer true then add class
+     */
+    if (hasShimmer) {
+      this.classList.add("hasShimmer");
+    }
+
+    /**
+     * classes to apply on icon , based on category
+     */
+    const iconClasses = {
+      "fill-button-surface": true,
+    };
+    /**
+     * create iconLeft if available
+     */
+    const iconLeft = this.iconLeft
+      ? html`<f-icon
+          .source=${this.iconLeft}
+          .state=${this.state}
+          class=${classMap({ "left-icon": true, ...iconClasses })}
+          .size=${this.iconSize}
+        ></f-icon>`
+      : "";
+    /**
+     * create iconRight if available
+     */
+    const iconRight = this.iconRight
+      ? html`<f-icon
+          .source=${this.iconRight}
+          .state=${this.state}
+          class=${classMap({ "right-icon": true, ...iconClasses })}
+          .size=${this.iconSize}
+        ></f-icon>`
+      : "";
+
+    /**
+     * create counter if available
+     */
+    const counterClasses = {
+      "fill-button-surface": true,
+    };
+    const counter = this.counter
+      ? html`<f-counter
+          state="neutral"
+          .size=${this.size}
+          .label=${Number(this.counter)}
+          class=${classMap(counterClasses)}
+        ></f-counter>`
+      : "";
+    /**
+     * render loading if required
+     */
+    if (this.loading) {
+      return html`<div
+        class=${classMap({ "f-tag": true, hasShimmer })}
+        style=${this.applyStyles()}
+        ?label=${this.label ? true : false}
+        size=${this.size}
+        state=${this.state}
+        ?loading=${this.loading}
+        ?disabled=${this.disabled}
+        ?selected=${this.selected}
+      >
+        ${unsafeSVG(loader)}${this.label}
+      </div>`;
+    }
+
+    /**
+     * Final html to render
+     */
+    return html`<div
+      class=${classMap({ "f-tag": true, hasShimmer })}
+      style=${this.applyStyles()}
+      ?label=${this.label ? true : false}
+      size=${this.size}
+      state=${this.state}
+      ?loading=${this.loading}
+      ?disabled=${this.disabled}
+      ?selected=${this.selected}
+    >
+      ${iconLeft}${this.label}${counter}${iconRight}
+    </div>`;
+  }
+}
+
+/**
+ * Required for typescript
+ */
+declare global {
+  interface HTMLElementTagNameMap {
+    "f-tag": FTag;
+  }
+}

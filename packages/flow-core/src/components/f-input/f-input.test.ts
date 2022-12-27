29fd7a26639c3d6839a2afd0f4267f77274cf0b8
Mayank fulera <fuleramayank@gmail.com>
2022-12-27 23:49:56 +0530

    f-input-changes

--- packages/flow-core/src/components/f-input/f-input.test.ts
+++ packages/flow-core/src/components/f-input/f-input.test.ts
@@ -4,7 +4,7 @@ import IconPack from "@cldcvr/flow-system-icon/dist/types/icon-pack";
 // import flow-core elements
 import "@cldcvr/flow-core";
 
-import { FButton, FIcon, ConfigUtil, FCounter, FInput, FText } from "@cldcvr/flow-core";
+import { FIcon, ConfigUtil, FInput, FText } from "@cldcvr/flow-core";
 // importing `loadingSVG` to cross check
 import loadingSVG from "../../mixins/svg/loader";
 
@@ -16,19 +16,6 @@ describe("f-input", () => {
     const el = document.createElement("f-input");
     expect(el).instanceOf(FInput);
   });
-  // it("should render label in default slot", async () => {
-  //   const el = await fixture(html` <f-input></f-input> `);
-  //   const descendant = el.shadowRoot!.querySelector(".f-input")!;
-  //   expect(descendant.textContent?.trim()).to.equal("abc");
-  // });
-  // it("should throw error", async () => {
-  //   try {
-  //     await fixture(html` <f-input></f-input>`);
-  //   } catch (e) {
-  //     expect((e as Error).message).to.equal("f-input : label is mandatory field");
-  //   }
-  // });
-
   it("should render with all default properties", async () => {
     const el = await fixture(html` <f-input></f-input> `);
     const descendant = el.shadowRoot!.querySelector(".f-input")!;
@@ -37,7 +24,6 @@ describe("f-input", () => {
     expect(descendant.getAttribute("state")).to.equal("inherit");
     expect(descendant.getAttribute("type")).to.equal("text");
   });
-
   it("should render icon left", async () => {
     const el = await fixture(html` <f-input icon-left="i-plus"></f-input> `);
     const descendant = el.shadowRoot!.querySelector(".f-input-prefix")!;
@@ -57,11 +43,43 @@ describe("f-input", () => {
     expect(ftext).instanceOf(FText);
     expect(ftext.textContent?.trim()).equal("abc");
   });
-  // it("should render loader", async () => {
-  //   const el = await fixture(html` <f-input label="abc" loading></f-input> `);
-  //   const descendant = el.shadowRoot!.querySelector(".f-input")!;
-  //   const loading = descendant.children[0];
-  //   const svg = await fixture(loadingSVG);
-  //   expect(loading.outerHTML).equal(svg.outerHTML);
-  // });
+  it("should render with suffix string", async () => {
+    const el = await fixture(html` <f-input suffix="abc"></f-input> `);
+    const descendant = el.shadowRoot!.querySelector(".f-input-suffix")!;
+    const ftext = descendant.children[0].children[0];
+    expect(ftext).instanceOf(FText);
+    expect(ftext.textContent?.trim()).equal("abc");
+  });
+  it("should render with prefix string and prefix icon", async () => {
+    const el = await fixture(html` <f-input prefix="abc" icon-left="i-app"></f-input> `);
+    const descendant = el.shadowRoot!.querySelector(".f-input-prefix")!;
+    const ftext = descendant.children[0].children[0];
+    const ficon = descendant.children[1];
+    expect(ftext).instanceOf(FText);
+    expect(ficon).instanceOf(FIcon);
+    expect(ftext.textContent?.trim()).equal("abc");
+  });
+  it("should render with suffix string and suffix icon", async () => {
+    const el = await fixture(html` <f-input suffix="abc" icon-right="i-app"></f-input> `);
+    const descendant = el.shadowRoot!.querySelector(".f-input-suffix")!;
+    const ftext = descendant.children[0].children[0];
+    const ficon = descendant.children[1];
+    expect(ftext).instanceOf(FText);
+    expect(ficon).instanceOf(FIcon);
+    expect(ftext.textContent?.trim()).equal("abc");
+  });
+  it("should render clear icon at right side", async () => {
+    const el = await fixture(html` <f-input value="abc" ?clear=${true}></f-input> `);
+    const descendant = el.shadowRoot!.querySelector(".f-input-suffix")!;
+    const icon = descendant.children[0];
+    expect(icon).instanceOf(FIcon);
+    expect(icon.getAttribute("source")).to.equal("i-close");
+  });
+  it("should render loader", async () => {
+    const el = await fixture(html` <f-input value="abc" ?loading=${true}></f-input> `);
+    const descendant = el.shadowRoot!.querySelector(".loader-suffix")!;
+    const loading = descendant.children[0];
+    const svg = await fixture(loadingSVG);
+    expect(loading.outerHTML).equal(svg.outerHTML);
+  });
 });

08f8380b7b3c59410cf3e44638a2e5775cfa4cd6
vikas-cldcvr <vikas@cldcvr.com>
2023-01-31 15:32:19 +0530

    :bug: FLOW-621 types extraction bug fixed

--- packages/custom-elements-manifest-to-types/index.ts
+++ packages/custom-elements-manifest-to-types/index.ts
@@ -217,13 +217,13 @@ function getComponentPropTypeImports(schema: Package, modulePath?: string): stri
 	const moduleTypeImports: string[] = [];
 	schema.modules.forEach((module) => {
 		const moduleName = modulePath || "./src"; //module.path.slice(0, -3);
+		const extractedTypes: Set<string> = new Set();
 		module.declarations?.forEach((declaration) => {
 			if (!("customElement" in declaration) || !declaration.customElement) {
 				return null;
 			}
 
 			if (declaration.attributes) {
-				const extractedTypes: Set<string> = new Set();
 				declaration.attributes.forEach((attribute) => {
 					if (attribute.type?.text) {
 						const typesToImport: string[] = attribute.type.text.split(" ");
@@ -234,17 +234,16 @@ function getComponentPropTypeImports(schema: Package, modulePath?: string): stri
 						});
 					}
 				});
-
-				if (extractedTypes.size > 0) {
-					let importStatement = `import type { `;
-					Array.from(extractedTypes).forEach((et, idx) => {
-						importStatement += `${et}${idx < extractedTypes.size - 1 ? "," : ""}`;
-					});
-					importStatement += `} from '${moduleName}';`;
-					moduleTypeImports.push(importStatement);
-				}
 			}
 		});
+		if (extractedTypes.size > 0) {
+			let importStatement = `import type { `;
+			Array.from(extractedTypes).forEach((et, idx) => {
+				importStatement += `${et}${idx < extractedTypes.size - 1 ? "," : ""}`;
+			});
+			importStatement += `} from '${moduleName}';`;
+			moduleTypeImports.push(importStatement);
+		}
 	});
 
 	return moduleTypeImports;

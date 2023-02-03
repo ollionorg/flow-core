44c34765773e6917c9d3aeca5f5f5a8a4a9a14c0
Mayank fulera <fuleramayank@gmail.com>
2023-02-03 14:01:31 +0530

    f-tooltip changes

--- packages/flow-core/src/components/f-tooltip/f-tooltip.test.ts
+++ packages/flow-core/src/components/f-tooltip/f-tooltip.test.ts
@@ -10,15 +10,8 @@ describe("f-tooltip", () => {
   });
   it("should render open tooltip ", async () => {
     const el = await fixture(html` <f-button label="add" tooltip="This is a tooltip"></f-button> `);
-    // expect(el.textContent?.trim()).to.equal("Test");
-    // const descendant = el.shadowRoot!.querySelector(".f-overlay")!;
-    // expect(descendant).not.null;
     const tooltip = document.querySelector<FTooltip>("#flow-tooltip");
-    if (tooltip) {
-      const isOpen = (tooltip.open = true);
-      if (isOpen) {
-        console.log(tooltip.shadowRoot!.querySelector("f-popover"));
-      }
-    }
+    console.log(tooltip.innerHTML);
+    expect(tooltip).not.null;
   });
 });

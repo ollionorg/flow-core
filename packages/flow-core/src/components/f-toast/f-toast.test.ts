022c8ad08d45fc3a5f95b8ccba5fdeb218d8b1fe
Mayank fulera <fuleramayank@gmail.com>
2023-03-05 18:17:19 +0530

    version changed

--- packages/flow-core/src/components/f-toast/f-toast.test.ts
+++ packages/flow-core/src/components/f-toast/f-toast.test.ts
@@ -1,10 +1,10 @@
-import { html, fixture, expect, oneEvent } from "@open-wc/testing";
+import { html, fixture, expect } from "@open-wc/testing";
 import IconPack from "@cldcvr/flow-system-icon/dist/types/icon-pack";
 
 // import flow-core elements
 import "@cldcvr/flow-core";
 
-import { FIcon, ConfigUtil, FToast, FText } from "@cldcvr/flow-core";
+import { FIcon, ConfigUtil, FToast } from "@cldcvr/flow-core";
 
 // setting icon pack for testing icon related test cases
 ConfigUtil.setConfig({ iconPack: IconPack });

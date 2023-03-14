84349656a844599eeccfb4754eafbd8903d9f212
vikas-cldcvr <vikas@cldcvr.com>
2023-03-14 17:05:18 +0530

    FLOW-706 sample update with network data

--- packages/flow-force-graph/src/components/f-force-graph/f-force-graph.ts
+++ packages/flow-force-graph/src/components/f-force-graph/f-force-graph.ts
@@ -3,9 +3,9 @@ import { customElement, query } from "lit/decorators.js";
 import { FRoot } from "@cldcvr/flow-core/src/mixins/components/f-root/f-root";
 import eleStyle from "./f-force-graph.scss";
 import * as d3 from "d3";
-import sampleData from "./sample-data.json";
+
 import { unsafeSVG } from "lit-html/directives/unsafe-svg.js";
-import { BaseType, SimulationNodeDatum } from "d3";
+import { BaseType, SimulationLinkDatum, SimulationNodeDatum } from "d3";
 
 @customElement("f-force-graph")
 export class FForceGraph extends FRoot {
@@ -22,32 +22,55 @@ export class FForceGraph extends FRoot {
 		const width = document.body.offsetWidth;
 		const height = document.body.offsetHeight;
 
-		const root = d3.hierarchy(sampleData);
-		const links = root.links() as d3.SimulationLinkDatum<d3.SimulationNodeDatum>[];
-		const nodes = root.descendants() as d3.SimulationNodeDatum[];
+		type ForceNode = SimulationNodeDatum & { id: number };
+		const dataset: {
+			nodes: ForceNode[];
+			links: SimulationLinkDatum<ForceNode>[];
+		} = {
+			nodes: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 61 }],
+			links: [
+				{ source: 1, target: 5 },
+				{ source: 4, target: 5 },
+				{ source: 4, target: 61 },
+				{ source: 3, target: 2 },
+				{ source: 5, target: 2 },
+				{ source: 1, target: 2 },
+				{ source: 3, target: 4 }
+			]
+		};
 
 		const simulation = d3
-			.forceSimulation(nodes)
+			.forceSimulation(dataset.nodes)
 			.force(
 				"link",
 				d3
-					.forceLink(links)
-					.id(d => d.index as number)
-					.distance(50)
-					.strength(0.3)
+					.forceLink(dataset.links)
+					.id(d => (d as ForceNode).id)
+					.distance(200)
+					.strength(0.5)
 			)
-			.force("charge", d3.forceManyBody().strength(-200))
+			.force("charge", d3.forceManyBody().distanceMin(100).strength(-5000))
 			.force("x", d3.forceX())
-			.force("y", d3.forceY());
+			.force("y", d3.forceY())
+			.alpha(1);
 
 		const svg = d3.select(this.svg).attr("viewBox", [-width / 2, -height / 2, width, height]);
 
-		const link = svg
+		const graphContainer = svg.append("g");
+		const handleZoom = (e: any) => {
+			graphContainer.attr("transform", e.transform);
+		};
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const zoom = d3.zoom().scaleExtent([0.3, 4]).on("zoom", handleZoom) as any;
+
+		svg.call(zoom).on("dblclick.zoom", null);
+
+		const link = graphContainer
 			.append("g")
 			.attr("stroke", "#999")
 			.attr("stroke-opacity", 0.6)
 			.selectAll("line")
-			.data(links)
+			.data(dataset.links)
 			.join("line");
 
 		function dragstarted(event: any, d: any) {
@@ -68,29 +91,28 @@ export class FForceGraph extends FRoot {
 		}
 
 		const drag = d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended) as (
-			selection: d3.Selection<
-				BaseType | SVGCircleElement,
-				SimulationNodeDatum,
-				SVGGElement,
-				unknown
-			>,
+			selection: d3.Selection<BaseType | SVGCircleElement, ForceNode, SVGGElement, unknown>,
 			...args: any[]
 		) => void;
 
-		const node = svg
+		const node = graphContainer
 			.append("g")
-			.attr("fill", "#fff")
-			.attr("stroke", "#000")
-			.attr("stroke-width", 1.5)
 			.selectAll("circle")
-			.data(nodes)
+			.data(dataset.nodes)
 			.join("circle")
-			.attr("fill", (d: any) => (d.children ? null : "var(--color-border-default)"))
-			.attr("stroke", (d: any) => (d.children ? null : "var(--color-surface-default)"))
-			.attr("r", 20)
+			.attr("fill", "var(--color-border-default)")
+			.attr("stroke", "var(--color-surface-default)")
+			.attr("r", 30)
 			.call(drag);
 
-		node.append("title").text((d: any) => d.data.name);
+		const text = graphContainer
+			.append("g")
+			.selectAll("text")
+			.data(dataset.nodes)
+			.join("text")
+			.text(d => d.id)
+			.attr("fill", "var(--color-text-default)")
+			.call(drag);
 
 		simulation.on("tick", () => {
 			link
@@ -100,9 +122,8 @@ export class FForceGraph extends FRoot {
 				.attr("y2", (d: any) => d.target.y);
 
 			node.attr("cx", (d: any) => d.x).attr("cy", (d: any) => d.y);
+			text.attr("x", (d: any) => d.x - 5).attr("y", (d: any) => d.y + 5);
 		});
-
-		//invalidation.then(() => simulation.stop());
 	}
 	render() {
 		return html` ${unsafeSVG(`<svg xmlns="http://www.w3.org/2000/svg"></svg>`)}`;

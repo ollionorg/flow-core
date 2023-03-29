0ecd0adf8efcb2fc4cc5a3f5ed2ac25e8f35cc9c
Mayank fulera <fuleramayank@gmail.com>
2023-03-29 11:45:03 +0530

    test

--- packages/flow-core/src/components/f-date-time-picker/f-date-time-picker.test.ts
+++ packages/flow-core/src/components/f-date-time-picker/f-date-time-picker.test.ts
@@ -1,20 +1,32 @@
-import { html, fixture, expect } from "@open-wc/testing";
+import { html, fixture, expect, oneEvent } from "@open-wc/testing";
+import IconPack from "@cldcvr/flow-system-icon/dist/types/icon-pack";
+
 // importing flow-core components
 import "@cldcvr/flow-core";
 
-import { FDivider } from "@cldcvr/flow-core";
+import { FDateTimePicker, ConfigUtil } from "@cldcvr/flow-core";
 
-describe("f-divider", () => {
-  it("is defined", () => {
-    const el = document.createElement("f-divider");
-    expect(el).instanceOf(FDivider);
-  });
+// setting icon pack for testing icon related test cases
+ConfigUtil.setConfig({ iconPack: IconPack });
 
-  it("should render with all default properties", async () => {
-    const el = await fixture(html` <f-divider></f-divider> `);
+describe("f-date-time-picker", () => {
+	it("is defined", () => {
+		const el = document.createElement("f-date-time-picker");
+		expect(el).instanceOf(FDateTimePicker);
+	});
 
-    expect(el.getAttribute("variant")).to.equal("solid");
-    expect(el.getAttribute("size")).to.equal("medium");
-    expect(el.getAttribute("state")).to.equal("default");
-  });
+	it("should render with all default properties", async () => {
+		const el = await fixture(html` <f-date-time-picker></f-date-time-picker> `);
+
+		expect(el.getAttribute("variant")).to.equal("curved");
+		expect(el.getAttribute("category")).to.equal("fill");
+		expect(el.getAttribute("mode")).to.equal("date-time");
+		expect(el.getAttribute("size")).to.equal("medium");
+		expect(el.getAttribute("state")).to.equal("default");
+	});
+	it("check if date picker renders", async () => {
+		const el = await fixture<FDateTimePicker>(html` <f-date-time-picker></f-date-time-picker> `);
+		const flatpickr = document.body.querySelector(".flatpickr-calendar");
+		expect(flatpickr?.classList.contains("open")).equals(false);
+	});
 });

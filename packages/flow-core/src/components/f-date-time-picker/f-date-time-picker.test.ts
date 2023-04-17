ff2ad961a8b2e389f0e228383513f402b2aea4d4
Mayank fulera <fuleramayank@gmail.com>
2023-04-18 00:38:17 +0530

    mode test case

--- packages/flow-core/src/components/f-date-time-picker/f-date-time-picker.test.ts
+++ packages/flow-core/src/components/f-date-time-picker/f-date-time-picker.test.ts
@@ -25,16 +25,40 @@ describe("f-date-time-picker", () => {
 		expect(el.getAttribute("state")).to.equal("default");
 	});
 	it("check if date picker renders", async () => {
-		const el = await fixture<FDateTimePicker>(html` <f-date-time-picker></f-date-time-picker> `);
+		await fixture<FDateTimePicker>(html` <f-date-time-picker></f-date-time-picker> `);
 		const flatpickr = document.body.querySelector(".flatpickr-calendar");
 		expect(flatpickr?.classList.contains("open")).equals(false);
 	});
-	it("check if date-time both are present", async () => {
-		const el = await fixture<FDateTimePicker>(
+	it("check if mode date-time both are present", async () => {
+		await fixture<FDateTimePicker>(
 			html` <f-date-time-picker mode="date-time"></f-date-time-picker> `
 		);
 		const flatpickr = document.body.querySelector(".flatpickr-calendar");
-		console.log(flatpickr?.classList.contains("hasTime"));
 		expect(flatpickr?.classList.contains("hasTime")).equals(true);
+	});
+
+	it("check if value selected in calendar", async () => {
+		await fixture<FDateTimePicker>(
+			html` <f-date-time-picker .value=${new Date()}></f-date-time-picker> `
+		);
+		const flatpickrToday = document.body.querySelector(".today");
+		const months = [
+			"January",
+			"February",
+			"March",
+			"April",
+			"May",
+			"June",
+			"July",
+			"August",
+			"September",
+			"October",
+			"November",
+			"December"
+		];
+		const dateFormat =
+			months[new Date().getMonth()] + " " + new Date().getDate() + ", " + new Date().getFullYear();
+		console.log(dateFormat);
+		expect(flatpickrToday?.getAttribute("aria-label")).to.equal(dateFormat);
 	});
 });

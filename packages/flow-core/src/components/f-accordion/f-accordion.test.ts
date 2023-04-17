7c0806f1c6b558f4b10cc279594f19a86a078be3
Mayank fulera <fuleramayank@gmail.com>
2023-04-17 12:06:30 +0530

    FLOW-741 | f-accordion (#62)
    * changes
    
    * f-accordion
    
    * verion changed
    
    * changes f-accordion max-height

---
+++ packages/flow-core/src/components/f-accordion/f-accordion.test.ts
@@ -0,0 +1,35 @@
+import { html, fixture, expect } from "@open-wc/testing";
+// importing flow-core components
+import "@cldcvr/flow-core";
+
+import { FAccordion, FIconButton } from "@cldcvr/flow-core";
+
+describe("f-accordion", () => {
+	it("is defined", () => {
+		const el = document.createElement("f-accordion");
+		expect(el).instanceOf(FAccordion);
+	});
+
+	it("should render with all default properties", async () => {
+		const el = await fixture(html` <f-accordion></f-accordion> `);
+
+		expect(el.getAttribute("icon")).to.equal("chevron");
+		expect(el.getAttribute("icon-size")).to.equal("small");
+		expect(el.getAttribute("icon-placement")).to.equal("right");
+	});
+
+	it("should render with i-caret-down/up as icon", async () => {
+		const el = await fixture(html` <f-accordion icon="caret" ?open=${true}></f-accordion> `);
+		const descendant = el.shadowRoot!.querySelector(".f-accordion-icon")!;
+		expect(descendant.getAttribute("icon")).to.equal("i-caret-down");
+	});
+
+	it("should render icon at left position", async () => {
+		const el = await fixture(
+			html` <f-accordion icon="caret" ?open=${true} icon-placement="left"></f-accordion> `
+		);
+		const descendant = el.shadowRoot!.querySelector(".f-accordion-header")!;
+		const icon = descendant.children[0].children[0];
+		expect(icon).instanceOf(FIconButton);
+	});
+});

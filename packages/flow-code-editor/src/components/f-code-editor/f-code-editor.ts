fed6f94559014a7ae35ac70e0703280966cd5d1f
vikas-cldcvr <vikas@cldcvr.com>
2023-05-22 17:43:58 +0530

    FLOW-774 language and code property added

--- packages/flow-code-editor/src/components/f-code-editor/f-code-editor.ts
+++ packages/flow-code-editor/src/components/f-code-editor/f-code-editor.ts
@@ -8,6 +8,7 @@ import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
 import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
 import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
 import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
+import { property } from "lit/decorators.js";
 
 self.MonacoEnvironment = {
 	getWorker(_, label) {
@@ -27,6 +28,16 @@ self.MonacoEnvironment = {
 	}
 };
 
+export type FCodeEditorLanguage =
+	| "scala"
+	| "python"
+	| "json"
+	| "css"
+	| "scss"
+	| "less"
+	| "html"
+	| "javascript"
+	| "typescript";
 @flowElement("f-code-editor")
 export class FCodeEditor extends FRoot {
 	/**
@@ -36,138 +47,28 @@ export class FCodeEditor extends FRoot {
 
 	editor?: monaco.editor.IStandaloneCodeEditor;
 
+	@property({ type: String })
+	code?: string;
+
+	@property({ reflect: true, type: String })
+	language?: FCodeEditorLanguage;
+
 	createRenderRoot() {
 		return this;
 	}
 
+	protected willUpdate(changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
+		super.willUpdate(changedProperties);
+		if (this.editor) {
+			this.editor.dispose();
+		}
+	}
 	protected updated(changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
 		super.updated(changedProperties);
-		const value = `// Example class
-		class Person {
-		  private name: string;
-		  private age: number;
-		
-		  constructor(name: string, age: number) {
-			this.name = name;
-			this.age = age;
-		  }
-		
-		  greet(): void {
-			console.log(\`Hello, my name is \${this.name} and I'm \${this.age} years old.\`);
-		  }
-		}
-		
-		// Create instances of Person
-		const person1 = new Person("Alice", 25);
-		const person2 = new Person("Bob", 30);
-		
-		// Call the greet method
-		person1.greet();
-		person2.greet();
-		
-		// Example function with type annotations
-		function addNumbers(a: number, b: number): number {
-		  return a + b;
-		}
-		
-		// Call the function
-		const result = addNumbers(5, 10);
-		console.log("The result is:", result);
-		
-		// Example interface
-		interface Animal {
-		  name: string;
-		  sound: string;
-		  makeSound(): void;
-		}
-		
-		// Implement the Animal interface
-		class Dog implements Animal {
-		  name: string;
-		  sound: string;
-		
-		  constructor(name: string) {
-			this.name = name;
-			this.sound = "Woof!";
-		  }
-		
-		  makeSound(): void {
-			console.log(this.sound);
-		  }
-		}
-		
-		// Create an instance of Dog
-		const dog = new Dog("Buddy");
-		
-		// Call the makeSound method
-		dog.makeSound();
-		
-		// Example generic function
-		function reverseArray<T>(array: T[]): T[] {
-		  return array.reverse();
-		}
-		
-		// Call the generic function
-		const numbers = [1, 2, 3, 4, 5];
-		const reversedNumbers = reverseArray(numbers);
-		console.log("Reversed numbers:", reversedNumbers);
-		
-		// Example class with inheritance
-		class Vehicle {
-		  protected brand: string;
-		
-		  constructor(brand: string) {
-			this.brand = brand;
-		  }
-		
-		  honk(): void {
-			console.log("Honk honk!");
-		  }
-		}
-		
-		// Subclass of Vehicle
-		class Car extends Vehicle {
-		  private model: string;
-		
-		  constructor(brand: string, model: string) {
-			super(brand);
-			this.model = model;
-		  }
-		
-		  drive(): void {
-			console.log(\`Driving a \${this.brand} \${this.model}.\`);
-		  }
-		}
-		
-		// Create an instance of Car
-		const car = new Car("Toyota", "Camry");
-		
-		// Call methods from both Vehicle and Car
-		car.honk();
-		car.drive();
-		
-		// Example type alias
-		type Point = {
-		  x: number;
-		  y: number;
-		};
-		
-		// Function using the Point type alias
-		function calculateDistance(point1: Point, point2: Point): number {
-		  const dx = point2.x - point1.x;
-		  const dy = point2.y - point1.y;
-		  return Math.sqrt(dx * dx + dy * dy);
-		}
-		
-		// Call the function
-		const distance = calculateDistance({ x: 0, y: 0 }, { x: 3, y: 4 });
-		console.log("Distance:", distance);
-		`;
-
 		this.editor = monaco.editor.create(this, {
-			value,
+			value: this.code,
 			theme: "vs-dark",
-			language: "typescript",
+			language: this.language,
 			automaticLayout: true,
 			autoDetectHighContrast: false,
 			readOnly: false,

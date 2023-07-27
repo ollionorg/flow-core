f664898dcccdfff38e8a0029c1222cb715a154d7
Mayank fulera <fuleramayank@gmail.com>
2023-07-27 16:17:36 +0530

    FLOW-836 | f-pictogram should have an auto-bg flag where it auto assigns a color based on characters #97 (#106)
    * f-pictogram auto-bg
    
    * fix color variable outside class

--- packages/flow-core/src/components/f-pictogram/f-pictogram.ts
+++ packages/flow-core/src/components/f-pictogram/f-pictogram.ts
@@ -1,9 +1,9 @@
-import { html, unsafeCSS } from "lit";
-import { property } from "lit/decorators.js";
+import { html, PropertyValueMap, unsafeCSS } from "lit";
+import { property, query } from "lit/decorators.js";
 import eleStyle from "./f-pictogram.scss";
 import { FRoot } from "../../mixins/components/f-root/f-root";
 import { ConfigUtil } from "@cldcvr/flow-core-config";
-import { isValidHttpUrl } from "./../../utils";
+import { getTextContrast, isValidHttpUrl } from "./../../utils";
 import { unsafeHTML } from "lit-html/directives/unsafe-html.js";
 import { classMap } from "lit-html/directives/class-map.js";
 import { FIcon } from "../f-icon/f-icon";
@@ -16,6 +16,32 @@ const states = ["primary", "danger", "warning", "success", "default", "inherit"]
 export type FPictogramVariant = (typeof variants)[number];
 export type FPictogramSize = (typeof sizes)[number];
 export type FPictogramState = (typeof states)[number];
+
+let colors = [
+	"#FFB900",
+	"#D83B01",
+	"#B50E0E",
+	"#E81123",
+	"#B4009E",
+	"#5C2D91",
+	"#0078D7",
+	"#00B4FF",
+	"#008272",
+	"#107C10"
+];
+
+function generateHslaColors(saturation: number, lightness: number, alpha: number, amount: number) {
+	const colors = [];
+	const huedelta = Math.trunc(360 / amount);
+
+	for (let i = 0; i < amount; i++) {
+		const hue = i * huedelta;
+		colors.push(`hsla(${hue},${saturation}%,${lightness}%,${alpha})`);
+	}
+	return colors;
+}
+
+colors = generateHslaColors(50, 60, 1.0, 10);
 
 @flowElement("f-pictogram")
 export class FPictogram extends FRoot {
@@ -60,22 +86,89 @@ export class FPictogram extends FRoot {
 	@property({ reflect: true, type: Boolean })
 	disabled?: boolean = false;
 
+	@query(".f-pictogram")
+	fPicorgramWrapper!: HTMLDivElement;
+
 	/**
 	 * @attribute The hover attribute to change background on hovering on pictogram.
 	 */
 	@property({ reflect: true, type: Boolean })
 	clickable?: boolean = false;
 
+	@property({ reflect: true, type: Boolean, attribute: "auto-bg" })
+	autoBg = false;
+
+	isText = false;
+
+	get getLetters() {
+		const acronym = this.source
+			.split(/\s/)
+			.reduce((response, word) => (response += word.slice(0, 1)), "");
+		return acronym?.toUpperCase()?.slice(0, 2);
+	}
+
+	capitalizeFirstLetter(string: string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
+
+	get textSource() {
+		const sourceLength = this.source.split(" ").length;
+		if (sourceLength <= 1) {
+			return this.capitalizeFirstLetter(this.source?.slice(0, 2));
+		} else {
+			return this.getLetters;
+		}
+	}
+
+	hslToHex(h: number, s: number, l: number) {
+		l /= 100;
+		const a = (s * Math.min(l, 1 - l)) / 100;
+		const f = (n: number) => {
+			const k = (n + h / 30) % 12;
+			const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+			return Math.round(255 * color)
+				.toString(16)
+				.padStart(2, "0"); // convert to Hex and prefix "0" if needed
+		};
+		return `#${f(0)}${f(8)}${f(4)}`;
+	}
+
+	get textColor() {
+		if (!this.hashCode.includes("#")) {
+			const hsla = this.hashCode
+				.replace("hsla", "")
+				.replace(/\(|\)/g, "")
+				.split("%")
+				.join("")
+				.split(",");
+
+			const hex = this.hslToHex(Number(hsla[0]), Number(hsla[1]), Number(hsla[2]));
+			return getTextContrast(hex) === "dark-text" ? "#202a36" : "#fcfcfd";
+		} else {
+			return getTextContrast(this.hashCode) === "dark-text" ? "#202a36" : "#fcfcfd";
+		}
+	}
+
+	get textColorStyling() {
+		if (this.autoBg) {
+			return `color:${this.textColor} !important`;
+		}
+		return "";
+	}
+
 	// returns html where source is being as input
 	get renderedHtml() {
 		const emojiRegex = /\p{Extended_Pictographic}/u;
 		if (isValidHttpUrl(this.source)) {
+			this.isText = false;
 			return `<img src="${this.source}" />`;
 		} else if (emojiRegex.test(this.source)) {
+			this.isText = false;
 			return `<f-icon class="${"f-pictogram-" + this.size + "-emoji"}" source="${
 				this.source
 			}" size="${this.sourceSize()}"></f-icon>`;
 		} else {
+			this.isText = false;
 			const IconPack = ConfigUtil.getConfig().iconPack;
 			if (IconPack) {
 				const svg = IconPack[this.source];
@@ -86,7 +179,8 @@ export class FPictogram extends FRoot {
 				}
 			}
 		}
-		return `<p class="text-styling">${this.source?.slice(0, 2)}</p>`;
+		this.isText = true;
+		return `<p class="text-styling" style=${this.textColorStyling}>${this.textSource}</p>`;
 	}
 	// calculating computed source size according to the user input size
 	sourceSize() {
@@ -101,9 +195,37 @@ export class FPictogram extends FRoot {
 		}
 	}
 
-	render() {
-		const hasShimmer = (getComputedStyle(this, "::before") as any)["animation-name"] === "shimmer";
+	get hashCode() {
+		let sum = 0;
+		for (let i = 0; i < this.source.length; i++) {
+			sum += this.source.charCodeAt(i);
+		}
+		return colors[sum % colors.length];
+	}
 
+	stringReplaceAtIndex(str: string, index: number, chr: number) {
+		if (index > str.length - 1) return str;
+		return str.substring(0, index) + chr + str.substring(index + 1);
+	}
+
+	get hashCodeHover() {
+		let sum = 0;
+		for (let i = 0; i < this.source.length; i++) {
+			sum += this.source.charCodeAt(i);
+		}
+		const color = colors[sum % colors.length];
+		return this.stringReplaceAtIndex(color, color.length - 2, 0.7);
+	}
+
+	validateProperties() {
+		if (!this.source) {
+			throw new Error("f-pictogram : source is mandatory field");
+		}
+	}
+
+	render() {
+		this.validateProperties();
+		const hasShimmer = (getComputedStyle(this, "::before") as any)["animation-name"] === "shimmer";
 		if (hasShimmer) {
 			this.classList.add("hasShimmer");
 		}
@@ -120,6 +242,7 @@ export class FPictogram extends FRoot {
 				?disabled=${this.disabled}
 				?loading=${this.loading}
 				?clickable=${this.clickable}
+				?auto-bg=${this.autoBg}
 			>
 				${unsafeHTML(this.renderedHtml)}
 				${this.variant === "squircle"
@@ -135,6 +258,30 @@ export class FPictogram extends FRoot {
 					: null}
 			</div>
 		`;
+	}
+	protected async updated(
+		changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>
+	): Promise<void> {
+		super.updated(changedProperties);
+		if (this.fPicorgramWrapper && this.autoBg && this.isText) {
+			// Modify the background-color property
+			this.fPicorgramWrapper.style.setProperty("--after-background-color", this.hashCode);
+			this.fPicorgramWrapper.style.setProperty(
+				"--after-background-color-hover",
+				this.hashCodeHover
+			);
+			this.fPicorgramWrapper.style.setProperty("--before-background-color", this.hashCode);
+		} else {
+			this.fPicorgramWrapper.style.setProperty(
+				"--after-background-color",
+				"var(--color-neutral-subtle)"
+			);
+			this.fPicorgramWrapper.style.setProperty(
+				"--after-background-color-hover",
+				"var(--color-neutral-subtle-hover)"
+			);
+			this.fPicorgramWrapper.style.setProperty("--before-background-color", "");
+		}
 	}
 }
 

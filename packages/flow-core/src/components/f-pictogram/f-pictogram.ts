d6edc28bac70e96191e99ddb87850d32e7fd365f
Mayank fulera <fuleramayank@gmail.com>
2023-10-18 15:28:35 +0530

    FLOW-948 |  f-pictogram auto-bg bug (#171)
    * FLOW-948 pictogram auto-bg
    
    * FLOW-948 pictogram auto-bg version
    
    * FLOW-948 pictogram auto-bg changerd

--- packages/flow-core/src/components/f-pictogram/f-pictogram.ts
+++ packages/flow-core/src/components/f-pictogram/f-pictogram.ts
@@ -1,5 +1,5 @@
 import { html, unsafeCSS } from "lit";
-import { property, query } from "lit/decorators.js";
+import { property } from "lit/decorators.js";
 import eleStyle from "./f-pictogram.scss?inline";
 import globalStyle from "./f-pictogram-global.scss?inline";
 import { FRoot } from "../../mixins/components/f-root/f-root";
@@ -96,9 +96,6 @@ export class FPictogram extends FRoot {
 	@property({ reflect: true, type: Boolean })
 	disabled?: boolean = false;
 
-	@query(".f-pictogram")
-	fPicorgramWrapper!: HTMLDivElement;
-
 	/**
 	 * @attribute The hover attribute to change background on hovering on pictogram.
 	 */
@@ -107,8 +104,6 @@ export class FPictogram extends FRoot {
 
 	@property({ reflect: true, type: Boolean, attribute: "auto-bg" })
 	autoBg = false;
-
-	isText = false;
 
 	get getLetters() {
 		const acronym = this.source
@@ -171,15 +166,12 @@ export class FPictogram extends FRoot {
 	get renderedHtml() {
 		const emojiRegex = /\p{Extended_Pictographic}/u;
 		if (isValidHttpUrl(this.source)) {
-			this.isText = false;
 			return `<img src="${this.source}" />`;
 		} else if (emojiRegex.test(this.source)) {
-			this.isText = false;
 			return `<f-icon class="${"f-pictogram-" + this.size + "-emoji"}" source="${
 				this.source
 			}" size="${this.sourceSize()}"></f-icon>`;
 		} else {
-			this.isText = false;
 			const IconPack = ConfigUtil.getConfig().iconPack;
 			if (IconPack) {
 				const svg = IconPack[this.source];
@@ -190,8 +182,27 @@ export class FPictogram extends FRoot {
 				}
 			}
 		}
-		this.isText = true;
 		return `<p class="text-styling" state=${this.state} style=${this.textColorStyling} >${this.textSource}</p>`;
+	}
+
+	// check for if source is a normal text
+	get isSourceText() {
+		const emojiRegex = /\p{Extended_Pictographic}/u;
+		const IconPack = ConfigUtil.getConfig().iconPack;
+		let svg;
+		if (IconPack) {
+			svg = IconPack[this.source];
+		}
+		if (isValidHttpUrl(this.source)) {
+			return false;
+		}
+		if (emojiRegex.test(this.source)) {
+			return false;
+		}
+		if (svg) {
+			return false;
+		}
+		return true;
 	}
 	// calculating computed source size according to the user input size
 	sourceSize() {
@@ -229,7 +240,7 @@ export class FPictogram extends FRoot {
 	}
 
 	applyStyles() {
-		if (this.fPicorgramWrapper && this.autoBg && this.isText) {
+		if (this.autoBg && this.isSourceText) {
 			// Modify the background-color property
 			return `--after-background-color: ${this.hashCode}; --after-background-color-hover: ${this.hashCodeHover}; --before-background-color:${this.hashCode}`;
 		} else {

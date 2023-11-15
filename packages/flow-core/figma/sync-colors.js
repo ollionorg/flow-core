19f746bc0b721c6efdbf8183d2cef1d1a8043104
Mayank fulera <fuleramayank@gmail.com>
2023-11-15 23:24:03 +0530

    FLOW-963 quotes fix

--- packages/flow-core/figma/sync-colors.js
+++ packages/flow-core/figma/sync-colors.js
@@ -7,8 +7,8 @@ const prettier = require("prettier");
 const fs = require("fs");
 
 // Constants
-const THEME_FLOW_GEN = "flow-gen";
-const THEME_OLLION = "ollion";
+const THEME_FLOW_GEN = "f-open-source";
+const THEME_OLLION = "f-ollion";
 
 const rgbToHex = (r, g, b) => "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 
@@ -66,7 +66,7 @@ function generateTokenScss(colorTokens) {
 			tokenFileName,
 			prettier.format(scss, {
 				printWidth: 100,
-				singleQuote: true,
+				singleQuote: false,
 				tabWidth: 4,
 				parser: "css"
 			})
@@ -77,13 +77,17 @@ function generateTokenScss(colorTokens) {
 	}
 }
 
+/**
+ * This will generate /src/shared/_text-tokens.scss file and it is consumed in `f-root.scss and one more file to consume this tokens dynamically i.e f-text-variables-dynamic.scss`
+ * @param {*} textTokens Json object of variants, weights and size for text.
+ */
 function generateTextScss(textTokens) {
 	const tokenTextFileName = `${__dirname}/../src/components/f-text/_f-text-variables-dynamic.scss`;
 	const scssFile = `${__dirname}/../src/mixins/scss/_text-tokens.scss`;
 	let scss = "";
 
 	let scssContent = "";
-	scssContent += convertToSCSSSyntax(textTokens[THEME_FLOW_GEN]);
+	scssContent += createVariablesSCSS(textTokens[THEME_FLOW_GEN]);
 	Object.keys(textTokens).forEach(theme => {
 		scss += createTokenSCSS(textTokens[theme], theme);
 	});
@@ -93,7 +97,7 @@ function generateTextScss(textTokens) {
 			scssFile,
 			prettier.format(scss, {
 				printWidth: 100,
-				singleQuote: true,
+				singleQuote: false,
 				tabWidth: 4,
 				parser: "css"
 			})
@@ -108,7 +112,7 @@ function generateTextScss(textTokens) {
 			tokenTextFileName,
 			prettier.format(scssContent, {
 				printWidth: 100,
-				singleQuote: true,
+				singleQuote: false,
 				tabWidth: 4,
 				parser: "css"
 			})
@@ -119,69 +123,38 @@ function generateTextScss(textTokens) {
 	}
 }
 
-function generateTextTokens(textStyle, variants, weights, fontFamily) {
-	const text = textStyle.name.split("/");
-	const name = text[0];
-	const size = text[1];
-	const weight = text[2];
-	const styling = textStyle.style;
+/**
+ * This will generate /src/shared/_text-tokens.scss file and it is consumed in `f-root.scss and one more file to consume this tokens dynamically i.e f-text-variables-dynamic.scss`
+ * @param {*} textStyle Json object from API consisting of text type and styling.
+ */
+function generateTextTokens(textStyle, textTokens) {
+	const {
+		name,
+		style: { fontSize, lineHeightPx, fontWeight, fontFamily }
+	} = textStyle;
+	const [theme, tokenName, size, weight] = name.split("/");
 
-	if (!(name in variants)) {
-		variants[name] = {};
+	textTokens[theme] ??= {
+		variants: {},
+		weights: { medium: null, bold: null, regular: null },
+		fontFamily: { general: null, code: null }
+	};
+	textTokens[theme].variants[tokenName] ??= {};
+	textTokens[theme].variants[tokenName][size] ??= {
+		fontSize,
+		lineHeight: lineHeightPx,
+		fontFamily: fontFamily
+	};
+
+	if (textTokens[theme].variants[tokenName][size].weight === undefined) {
+		textTokens[theme].variants[tokenName][size].weight = {
+			medium: null,
+			bold: null,
+			regular: null
+		};
 	}
-	if (!(size in variants[name])) {
-		variants[name][size] = { fontSize: styling.fontSize, lineHeight: styling.lineHeightPx };
-	}
 
-	if (!(weight in weights)) {
-		weights[weight] = styling.fontWeight;
-	}
-
-	if (!("general" in fontFamily)) {
-		if (name !== "code") {
-			fontFamily.general = styling.fontFamily;
-		}
-	}
-	if (!("code" in fontFamily)) {
-		if (name === "code") {
-			fontFamily.code = styling.fontFamily;
-		}
-	}
-}
-
-function convertToSCSSSyntax(data) {
-	let scssContent = `\n$variants: (\n`;
-
-	Object.keys(data.variants).forEach(category => {
-		scssContent += `\t"${category}": (\n`;
-
-		Object.keys(data.variants[category]).forEach(size => {
-			scssContent += `\n\t\t"${size}": (\n`;
-			scssContent += `\t\t\t"fontSize": var(${fontTokenString(category, size)}),\n`;
-			scssContent += `\t\t\t"lineHeight": var(${lineHeightTokenString(category, size)})\n`;
-			scssContent += `\t\t),\n`;
-		});
-
-		scssContent += `\t),\n`;
-	});
-
-	scssContent += `);`;
-
-	scssContent += `\n$weights: (\n`;
-
-	Object.keys(data.weights).forEach((key, index, array) => {
-		scssContent += `\t"${key}": var(${weightTokenString(key)})`;
-
-		if (index !== array.length - 1) {
-			scssContent += ",";
-		}
-
-		scssContent += "\n";
-	});
-
-	scssContent += `);\n`;
-
-	return scssContent;
+	textTokens[theme].variants[tokenName][size].weight[weight] ??= fontWeight;
 }
 
 function fontTokenString(category, size) {
@@ -192,53 +165,145 @@ function lineHeightTokenString(category, size) {
 	return `--text-${category}-${size}-lineheight`;
 }
 
-function weightTokenString(key) {
-	return `--text-${key}-weight`;
+function newWeightTokenString(category, size, weight) {
+	return `--text-${category}-${size}-${weight}`;
 }
 
+function fontFamilyTokenString(category, size) {
+	return `--text-${category}-${size}-fontfamily`;
+}
+
+/**
+ *
+ * @param {*} data Json object of variants, weights and size for text
+ * @returns scss string formed for creating text-variable file
+ */
+function createVariablesSCSS(data) {
+	const scssContent = Object.keys(data.variants)
+		.map(category => {
+			const categoryContent = Object.entries(data.variants[category])
+				.map(([size, value]) => {
+					const fontSizeToken = fontTokenString(category, size);
+					const lineHeightToken = lineHeightTokenString(category, size);
+					const fontFamilyToken = fontFamilyTokenString(category, size);
+					return (
+						`\t\t"${size}": (\n` +
+						`\t\t\t"fontSize": var(${fontSizeToken}),\n` +
+						`\t\t\t"lineHeight": var(${lineHeightToken}),\n` +
+						`\t\t\t"weight": (\n` +
+						`${Object.keys(value.weight)
+							.map(item => {
+								const newWeightToken = newWeightTokenString(category, size, item);
+								return `\t\t\t\t"${item}": var(${newWeightToken}),\n`;
+							})
+							.join("")}` +
+						`\t\t\t),\n` +
+						`\t\t\t"fontFamily": var(${fontFamilyToken})\n` +
+						`\t\t),\n`
+					);
+				})
+				.join("");
+
+			return `\t"${category}": (\n${categoryContent}\t),\n`;
+		})
+		.join("");
+
+	return `\n$variants: (\n${scssContent});\n\n`;
+}
+
+/**
+ *
+ * @param {*} data Json object of variants, weights and size for text
+ * @param {*} theme theme for ollion or general flow-core, as we need top create separate tokens with separate values for both diffeent themes.
+ * @returns
+ */
 function createTokenSCSS(data, theme) {
-	let scssContent =
+	const themeSelector =
 		theme === THEME_OLLION
-			? `[data-theme="f-ollion-dark"], [data-theme="f-ollion-light"] {\n`
-			: `[data-theme="f-dark"], [data-theme="f-light"] {\n`;
+			? `[data-theme="f-ollion-dark"], [data-theme="f-ollion-light"]`
+			: `[data-theme="f-dark"], [data-theme="f-light"]`;
 
-	Object.keys(data.variants).forEach(category => {
-		Object.keys(data.variants[category]).forEach(size => {
-			const { fontSize, lineHeight } = data.variants[category][size];
-			scssContent += `${fontTokenString(category, size)} : ${fontSize}px;\t\n`;
-			scssContent += `${lineHeightTokenString(category, size)} : ${Math.round(lineHeight)}px;\t\n`;
-		});
-	});
-
-	Object.keys(data.weights).forEach(key => {
-		scssContent += `${weightTokenString(key)} : ${data.weights[key]};\t\n`;
-	});
-
-	scssContent += `--flow-font : ${
-		theme === THEME_OLLION ? `"${data.fontFamily.general}", serif` : `"Montserrat", sans-serif`
-	};\t\n`;
-	scssContent += `--flow-code-font : ${
-		theme === THEME_OLLION ? `"${data.fontFamily.code}", monospace` : `"Courier Prime", monospace`
-	};\t\n`;
-
-	scssContent += `\n}\n`;
+	const scssContent = `
+  ${themeSelector} {
+    ${generateCategoryStyles(data.variants)}
+  }\n`;
 
 	return scssContent;
 }
 
 /**
- * Get document of specified file Id
+ *
+ * @param {*} variants json object for variants of text and their font-sizes
+ * @returns stringified scss tokens for texts
  */
-async function processStyles(theme, nodeIds, colorTokens, textTokens) {
+function generateCategoryStyles(variants) {
+	return Object.keys(variants)
+		.map(category => generateSizeStyles(category, variants[category]))
+		.join("");
+}
+
+/**
+ *
+ * @param {*} category variant for the fontfamily
+ * @param {*} fontFamily name of font
+ * @returns proper font-family string creation
+ */
+function createFontFamily(category, fontFamily) {
+	return category === "code" ? `"${fontFamily}", monospace` : `"${fontFamily}", sans-serif`;
+}
+
+/**
+ *
+ * @param {*} category category includes - para, code, heading
+ * @param {*} sizes sizes values are different for every category
+ * @returns stringified scss tokens for text variants
+ */
+function generateSizeStyles(category, sizes) {
+	return Object.keys(sizes)
+		.map(size => {
+			const { fontSize, lineHeight, weight, fontFamily } = sizes[size];
+			return `${fontTokenString(category, size)}: ${fontSize}px;\t\n
+			${lineHeightTokenString(category, size)}: ${Math.round(lineHeight)}px;\t\n
+			${generateWeightStyles(weight, category, size)}
+			${fontFamilyTokenString(category, size)}: ${createFontFamily(category, fontFamily)};\t\n
+			`;
+		})
+		.join("");
+}
+
+/**
+ *
+ * @param {*} weights weights json holds - bold, medium, regular
+ * @returns stringified scss tokens for text weights
+ */
+function generateWeightStyles(weight, category, size) {
+	return Object.entries(weight)
+		.map(([w, value]) => {
+			return `
+						${newWeightTokenString(category, size, w)}: ${Math.round(value)};\t\n
+			`;
+		})
+		.join("");
+}
+
+/**
+ *
+ * @param {*} theme theme present is for ollion of general-flow-core
+ * @param {*} nodeIds list of nodeids
+ * @param {*} colorTokens Json object of theme and color variables
+ * @param {*} textTokens Json object of variants, weights and size for text.
+ */
+async function processStyles(nodeIds, colorTokens, textTokens) {
 	try {
-		const nodes = await getNode(nodeIds.join(","), theme);
-		const variants = {};
-		const weights = {};
-		const fontFamily = {};
+		const nodes = await getNode(nodeIds.join(","));
 
 		for (const [_id, obj] of Object.entries(nodes.data.nodes)) {
-			if (obj.document.type === "TEXT") {
-				generateTextTokens(obj.document, variants, weights, fontFamily);
+			const textTheme = obj.document.name.split("/")[0];
+			if (
+				obj.document.type === "TEXT" &&
+				(textTheme === THEME_FLOW_GEN || textTheme === THEME_OLLION)
+			) {
+				generateTextTokens(obj.document, textTokens);
 			} else {
 				if (obj.document.fills[0].color) {
 					const { r, g, b } = obj.document.fills[0].color;
@@ -256,40 +321,25 @@ async function processStyles(theme, nodeIds, colorTokens, textTokens) {
 				}
 			}
 		}
-		if (theme === THEME_OLLION) {
-			if (!(THEME_OLLION in textTokens)) {
-				textTokens[THEME_OLLION] = { variants: variants, weights: weights, fontFamily: fontFamily };
-			}
-		} else {
-			if (!(THEME_FLOW_GEN in textTokens)) {
-				textTokens[THEME_FLOW_GEN] = {
-					variants: variants,
-					weights: weights,
-					fontFamily: fontFamily
-				};
-			}
-		}
 	} catch (error) {
-		console.error(`Error processing styles for ${theme}:`, error);
+		console.error(`Error processing styles:`, error);
 		throw error;
 	}
 }
 
+/**
+ * Get document of specified file Id
+ */
 async function generateStyles() {
 	const colorTokens = {};
 	const textTokens = {};
 
 	try {
-		const [response1, response2] = await Promise.all([getStyles(), getStyles(THEME_OLLION)]);
-
-		const nodeids1 = response1.data.meta.styles.map(element => element.node_id);
-		const nodeids2 = response2.data.meta.styles.map(element => element.node_id);
+		const response = await getStyles();
+		const nodeids1 = response.data.meta.styles.map(element => element.node_id);
 
 		// Process and update colorTokens for the first set
-		await processStyles(THEME_FLOW_GEN, nodeids1, colorTokens, textTokens);
-
-		// Process and update colorTokens for the second set
-		await processStyles(THEME_OLLION, nodeids2, colorTokens, textTokens);
+		await processStyles(nodeids1, colorTokens, textTokens);
 
 		console.log("\n");
 		generateTokenScss(colorTokens);

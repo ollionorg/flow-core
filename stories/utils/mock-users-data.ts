aef0bd61d20a07ae37adfdf33b476111dc1f931e
vikas-cldcvr <vikas@cldcvr.com>
2023-11-29 15:53:41 +0530

    FLOW-968 align property added on f-tcell

--- stories/utils/mock-users-data.ts
+++ stories/utils/mock-users-data.ts
@@ -15,7 +15,7 @@ export default function getFakeUsers(rowCount = 100, columnCount = 8): FTableSch
 	const users = [];
 
 	for (let i = 0; i < rowCount; i++) {
-		const firstName = { value: faker.name.firstName() };
+		const firstName = { value: faker.name.firstName(), align: "middle-left" };
 		const lastName = {
 			value: faker.name.lastName(),
 			template: function () {

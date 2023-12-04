437d7ef65d1bfd8436cb09d9378fdd74ef6494d1
vikas-cldcvr <vikas@cldcvr.com>
2023-12-04 18:32:09 +0530

    FLOW-970 asyc await removed and used then

--- packages/flow-core/src/components/f-div/f-div.ts
+++ packages/flow-core/src/components/f-div/f-div.ts
@@ -350,7 +350,7 @@ export class FDiv extends FRoot {
 		return html` <slot></slot>${this.loading === "loader" ? html`${unsafeSVG(loader)}` : ""}`;
 	}
 
-	protected async updated(changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>) {
+	protected updated(changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>) {
 		super.updated(changedProperties);
 
 		if (
@@ -359,14 +359,16 @@ export class FDiv extends FRoot {
 		) {
 			this.checkHighlight();
 		}
-		await this.updateComplete;
-		if (this.variant === "round") {
-			this.style.borderRadius = `${this.offsetHeight / 2}px`;
-		} else if (this.variant === "curved") {
-			this.style.borderRadius = `4px`;
-		} else {
-			this.style.borderRadius = "0px";
-		}
+
+		void this.updateComplete.then(() => {
+			if (this.variant === "round") {
+				this.style.borderRadius = `${this.offsetHeight / 2}px`;
+			} else if (this.variant === "curved") {
+				this.style.borderRadius = `4px`;
+			} else {
+				this.style.borderRadius = "0px";
+			}
+		});
 	}
 }
 

abae13f913677bed94ebaba55a9290322303b568
vikas-cldcvr <vikas@cldcvr.com>
2023-12-11 17:47:49 +0530

    FLOW-972 unit tests added

--- packages/flow-log/src/components/f-log/f-log.test.ts
+++ packages/flow-log/src/components/f-log/f-log.test.ts
@@ -1,18 +1,45 @@
 import { html, fixture, expect } from "@open-wc/testing";
 
+import samplelogs from "./sample-logs";
+
 // import flow-core elements
 import "@cldcvr/flow-core";
 import { FLog } from "@cldcvr/flow-log";
+import { FDiv } from "@cldcvr/flow-core";
 
 describe("f-log", () => {
 	it("is defined", () => {
 		const el = document.createElement("f-log");
 		expect(el).instanceOf(FLog);
 	});
-	it("should render search box", async () => {
-		const el = await fixture(html` <f-log ?show-search=${true}></f-log> `);
-		const descendant = el.shadowRoot!.querySelector<HTMLDivElement>(".f-log")!;
-		const descendant1 = descendant?.querySelector<HTMLDivElement>(".xterm-search-bar__addon");
-		expect(descendant1?.style.visibility).to.equal("visible");
+	it("shouldn't render tool bar by default", async () => {
+		const el = await fixture(html` <f-log .logs=${samplelogs}></f-log> `);
+		const topBar = el.shadowRoot!.querySelector<FDiv>(".top-bar");
+		expect(topBar).is.null;
+	});
+	it("should render tool bar", async () => {
+		const el = await fixture(html` <f-log show-toolbar .logs=${samplelogs}></f-log> `);
+		const topBar = el.shadowRoot!.querySelector<FDiv>(".top-bar");
+		expect(topBar).instanceOf(FDiv);
+	});
+
+	it("should wrap lines", async () => {
+		const el = await fixture<FLog>(html` <f-log .logs=${samplelogs}></f-log> `);
+		await el.updateComplete;
+		let pre = el.shadowRoot!.querySelector<FDiv>("pre");
+		expect(pre.offsetHeight).is.equal(12976);
+		el.wrapText = true;
+		await el.updateComplete;
+		pre = el.shadowRoot!.querySelector<FDiv>("pre");
+		expect(pre.offsetHeight).is.equal(26544);
+	});
+
+	it("should display only selected log level", async () => {
+		const el = await fixture<FLog>(html`
+			<f-log .logs=${samplelogs} .selectedLogLevel=${"ERROR"}></f-log>
+		`);
+		await el.updateComplete;
+		const visibleLines = el.shadowRoot!.querySelectorAll<HTMLSpanElement>(".log-line:not(.hidden)");
+		expect(visibleLines.length).is.equal(57);
 	});
 });

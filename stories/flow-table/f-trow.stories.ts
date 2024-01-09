fc1e11144d5c1c608fb2fb6ae8568c046cf0383f
vikas-cldcvr <vikas@cldcvr.com>
2024-01-09 15:31:58 +0530

    FLOW-1072 badges fixed

--- stories/flow-table/f-trow.stories.ts
+++ stories/flow-table/f-trow.stories.ts
@@ -12,12 +12,12 @@ export default {
 };
 
 export const Playground = {
-	render: args => {
-		const handleSelectedRows = event => {
+	render: (args: Record<string, unknown>) => {
+		const handleSelectedRows = (event: CustomEvent) => {
 			console.log("selected rows", event.detail);
 		};
 
-		const testOverrideClick = event => {
+		const testOverrideClick = (event: CustomEvent) => {
 			console.log("clicked", event);
 		};
 
@@ -28,7 +28,7 @@ export const Playground = {
 						return html`<f-tcell @click=${testOverrideClick}> Header ${cellNumber} </f-tcell>`;
 					})}
 				</f-trow>
-				${[1, 2, 3, 4, 5].map(rowNumber => {
+				${[1, 2, 3, 4, 5].map(() => {
 					return html`<f-trow
 						.disableSelection=${args["disable-selection"]}
 						.state=${args.state}
@@ -90,7 +90,7 @@ export const Anatomy = {
 };
 
 export const State = {
-	render: args =>
+	render: () =>
 		html`<f-div direction="column" state="subtle" padding="small" gap="large"
 			>${["primary", "neutral", "success", "warning", "danger", "inherit", "default"].map(
 				state =>
@@ -101,7 +101,7 @@ export const State = {
 									return html`<f-tcell> Header ${cellNumber} </f-tcell>`;
 								})}
 							</f-trow>
-							${[1, 2, 3, 4, 5].map(rowNumber => {
+							${[1, 2, 3, 4, 5].map(() => {
 								return html`<f-trow .state=${state}>
 									${[1, 2, 3, 4, 5].map(cellNumber => {
 										return html`<f-tcell>
@@ -118,7 +118,7 @@ export const State = {
 };
 
 export const Open = {
-	render: args =>
+	render: () =>
 		html`<f-div direction="column" state="subtle" padding="small" gap="large"
 			>${[true, false].map(
 				open =>
@@ -129,7 +129,7 @@ export const Open = {
 									return html`<f-tcell> Header ${cellNumber} </f-tcell>`;
 								})}
 							</f-trow>
-							${[1, 2, 3, 4, 5].map(rowNumber => {
+							${[1, 2, 3, 4, 5].map(() => {
 								return html`<f-trow .open=${open}>
 									<f-div padding="large" slot="details">
 										<f-text variant="heading" size="x-large">This is details slot</f-text>
@@ -149,7 +149,7 @@ export const Open = {
 };
 
 export const Selected = {
-	render: args =>
+	render: () =>
 		html`<f-div direction="column" state="subtle" padding="small" gap="large"
 			>${[true, false].map(
 				selected =>
@@ -160,7 +160,7 @@ export const Selected = {
 									return html`<f-tcell> Header ${cellNumber} </f-tcell>`;
 								})}
 							</f-trow>
-							${[1, 2, 3, 4, 5].map(rowNumber => {
+							${[1, 2, 3, 4, 5].map(() => {
 								return html`<f-trow .selected=${selected}>
 									<f-div padding="large" slot="details">
 										<f-text variant="heading" size="x-large">This is details slot</f-text>
@@ -178,12 +178,38 @@ export const Selected = {
 
 	name: "selected"
 };
+export const ExpandIconPosition = {
+	render: () =>
+		html`<f-div direction="column" state="subtle" padding="small" gap="large"
+			><f-table>
+				<f-trow slot="header">
+					${[1, 2].map(cellNumber => {
+						return html`<f-tcell> Header ${cellNumber} </f-tcell>`;
+					})}
+				</f-trow>
+				${["left", "right"].map(iconPos => {
+					return html`<f-trow expand-icon-position="${iconPos}">
+						<f-div padding="large" slot="details">
+							<f-text variant="heading" size="x-large">This is details slot</f-text>
+						</f-div>
+						${[1, 2].map(() => {
+							return html`<f-tcell>
+								<f-text> ${iconPos} icon position </f-text>
+							</f-tcell>`;
+						})}
+					</f-trow>`;
+				})}
+			</f-table></f-div
+		>`,
+
+	name: "expand-icon-position"
+};
 
 export const SelectedRow = {
-	render: args => {
+	render: () => {
 		const fieldRef = createRef();
 
-		const selectedRowHandler = event => {
+		const selectedRowHandler = (event: CustomEvent) => {
 			if (fieldRef.value) {
 				fieldRef.value.textContent = JSON.stringify(event.detail.value, undefined, 2);
 			}
@@ -197,7 +223,7 @@ export const SelectedRow = {
 							return html`<f-tcell> Header ${cellNumber} </f-tcell>`;
 						})}
 					</f-trow>
-					${[1, 2, 3, 4, 5].map(rowNumber => {
+					${[1, 2, 3, 4, 5].map(() => {
 						return html`<f-trow @selected-row=${selectedRowHandler}>
 							<f-div padding="large" slot="details">
 								<f-text variant="heading" size="x-large">This is details slot</f-text>
@@ -223,10 +249,10 @@ export const SelectedRow = {
 };
 
 export const ToggleRow = {
-	render: args => {
+	render: () => {
 		const fieldRef = createRef();
 
-		const toggleRowHandler = event => {
+		const toggleRowHandler = (event: CustomEvent) => {
 			if (fieldRef.value) {
 				fieldRef.value.textContent = JSON.stringify(event.detail, undefined, 2);
 			}
@@ -240,7 +266,7 @@ export const ToggleRow = {
 							return html`<f-tcell> Header ${cellNumber} </f-tcell>`;
 						})}
 					</f-trow>
-					${[1, 2, 3, 4, 5].map(rowNumber => {
+					${[1, 2, 3, 4, 5].map(() => {
 						return html`<f-trow @toggle-row=${toggleRowHandler}>
 							<f-div padding="large" slot="details">
 								<f-text variant="heading" size="x-large">This is details slot</f-text>

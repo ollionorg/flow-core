0fb57b1dca9ce64ad7f00def26ec7da7bc2cc9e6
vikas-cldcvr <vikas@cldcvr.com>
2024-02-05 12:57:40 +0530

    FLOW-1220 a11y support

--- packages/flow-core/src/utils/index.ts
+++ packages/flow-core/src/utils/index.ts
@@ -7,6 +7,19 @@ import LightenDarkenColor from "./get-lighten-darken-color";
 import getFormattedBytes from "./get-formatted-bytes";
 import flowElement from "./flow-element";
 import getExtensionsFromMimeType from "./mime-extension-map";
+
+function generateId(length = 5) {
+	let result = "";
+	const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	const charactersLength = characters.length;
+	let counter = 0;
+	while (counter < length) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		counter += 1;
+	}
+	return result;
+}
+
 export {
 	isValidHttpUrl,
 	getTextContrast,
@@ -16,5 +29,6 @@ export {
 	LightenDarkenColor,
 	getFormattedBytes,
 	flowElement,
+	generateId,
 	getExtensionsFromMimeType
 };

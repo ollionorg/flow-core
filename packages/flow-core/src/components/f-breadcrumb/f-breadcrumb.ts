18867a22ec212f5c3f50d2af4df243921c678001
vikas-cldcvr <vikas@cldcvr.com>
2024-02-19 15:34:59 +0530

    FDS-290 f-breadcrumb accessibility

--- packages/flow-core/src/components/f-breadcrumb/f-breadcrumb.ts
+++ packages/flow-core/src/components/f-breadcrumb/f-breadcrumb.ts
@@ -17,11 +17,17 @@ const sizes = ["medium", "small"] as const;
 
 export type FBreadCrumbsProp = { tabIndex: number; title: string; icon?: string };
 export type FBreadcrumbs = FBreadCrumbsProp[];
-export type FBreadcrumbSize = (typeof sizes)[number];
-export type FBreadcrumbVariant = (typeof variants)[number];
+export type FBreadcrumbSize = typeof sizes[number];
+export type FBreadcrumbVariant = typeof variants[number];
 
 @flowElement("f-breadcrumb")
 export class FBreadcrumb extends FRoot {
+	constructor() {
+		super();
+
+		this.role = "navigation";
+		this.setAttribute("aria-label", "Breadcrumb");
+	}
 	/**
 	 * css loaded from scss file
 	 */

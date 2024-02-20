58dffdd9e0b9e0ecf2453213d00b85cc2be0246c
vikas-cldcvr <vikas@cldcvr.com>
2024-02-20 14:57:52 +0530

    FDS-290 a11y for f-checkbox f-radio f-progress-bar f-radio f-icon f-countdown

--- packages/flow-core/src/components/f-countdown/f-countdown.ts
+++ packages/flow-core/src/components/f-countdown/f-countdown.ts
@@ -16,10 +16,10 @@ const sizes = ["large", "medium", "small", "x-small"] as const;
 const categories = ["fill", "outline"] as const;
 const placements = ["left", "right", "bottom", "top", "none"] as const;
 
-export type FCountdownStateProp = typeof states[number];
-export type FCountdownCategoryProp = typeof categories[number];
-export type FCountdownSizesProp = typeof sizes[number];
-export type FCountdownLabelProp = typeof placements[number];
+export type FCountdownStateProp = (typeof states)[number];
+export type FCountdownCategoryProp = (typeof categories)[number];
+export type FCountdownSizesProp = (typeof sizes)[number];
+export type FCountdownLabelProp = (typeof placements)[number];
 export type FCountdownDuration = number | string;
 
 @flowElement("f-countdown")

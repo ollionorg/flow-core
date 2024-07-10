75b394e98f48f1f1373f1f48365cf53fd5991c5e
Vikas Awaghade <vikas.awaghade@hotmail.com>
2024-07-10 12:56:45 +0530

    flow-text-editor changelogs updated

--- packages/flow-text-editor/src/components/f-text-editor/f-text-editor.test.ts
+++ packages/flow-text-editor/src/components/f-text-editor/f-text-editor.test.ts
@@ -8,6 +8,9 @@ import { ConfigUtil } from "@ollion/flow-core";
 import { FTextEditor } from "@ollion/flow-text-editor";
 ConfigUtil.setConfig({ iconPack: IconPack });
 
+/**
+ * flow-text-editor is based on Quill so we added our skin and few props. hence for feature testing we don't need more test cases
+ */
 describe("f-text-editor", () => {
 	it("is defined", () => {
 		const el = document.createElement("f-text-editor");
